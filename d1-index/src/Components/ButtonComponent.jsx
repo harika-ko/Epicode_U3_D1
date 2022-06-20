de245564767fe8a5624d9f7f8d697076c16d9a72
Harika_Kommuri <harikakommuri35@gmail.com>
2022-06-20 13:54:21 +0200

    Added Button Component

---
+++ d1-index/src/Components/ButtonComponent.jsx
@@ -0,0 +1,9 @@
+const ButtonComponent = ({ button }) => {
+    return (
+        <div class="button">
+            <button type="button">{button}</button>
+        </div>
+    )
+}
+
+export default ButtonComponent
